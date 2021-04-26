5ffe2e95a4f2533b81dd6507e7143b77e005bbcc
Jon <jsmartware@aol.com>
2021-04-26 07:44:51 -0700

    Add project files.

---
+++ src/CreditCards/Startup.cs
@@ -0,0 +1,78 @@
+﻿using CreditCards.Core.Interfaces;
+using CreditCards.Infrastructure;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+
+namespace CreditCards
+{
+    public class Startup
+    {
+        private readonly IWebHostEnvironment _currentEnvironment;
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            Configuration = configuration;
+            _currentEnvironment = env;
+        }
+        
+        public IConfiguration Configuration { get; }       
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+            
+            services.AddScoped<ICreditCardApplicationRepository, EntityFrameworkCreditCardApplicationRepository>();
+
+            services.AddMvc();
+            
+        
+            if (_currentEnvironment.EnvironmentName == "Development")
+            {             
+                services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("inmem"));
+            }
+            else
+            {                
+                services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            }
+        }
+
+        
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();               
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            //app.UseMvc(routes =>
+            //{
+            //    routes.MapRoute(
+            //        name: "default",
+            //        template: "{controller=Home}/{action=Index}/{id?}");
+            //});
+
+            app.UseRouting();
+        }
+    }
+}
